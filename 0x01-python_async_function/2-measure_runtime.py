94cb3be85a3ca6c9bba29ae0f39c8e4776342c5e
CalvinKarume <johncalvinkarume@gmail.com>
2023-12-11 12:00:23 -0800

    Error kinda solved

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,29 +1,15 @@
 #!/usr/bin/env python3
-"""Measure the runtime"""
-
-import importlib
+"""Measure the Runtime"""
 import asyncio
 import time
 
-MODULE_NAME = '1-concurrent_coroutines'
-module = importlib.import_module(MODULE_NAME)
-wait_n = getattr(module, 'wait_n')
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-   """imports the wait_n coroutine from a previous file and defines measure_time,
- a function that measures the total execution time for wait_n(n, max_delay)""" 
-        
+       """imports the wait_n coroutine from a previous file and defines measure_time,
+ a function that measures the total execution time for wait_n(n, max_delay)"""
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
-    return total_time / n
-
-if __name__ == "__main__":
-    # Example usage
-    n = 5
-    max_delay = 3
-    avg_time = measure_time(n, max_delay)
-    print(f"Average time per operation: {avg_time} seconds")
-
+    return (time.time() - start_time) / n
